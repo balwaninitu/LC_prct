be0d86749ed7d0aee380838a00881f86b83bae96
Nitu Balwani <balwaninitu@gmail.com>
2023-04-23 22:05:27 +0800

    7 que comments

--- LC/src/ArrayOfLongestString.java
+++ LC/src/ArrayOfLongestString.java
@@ -37,3 +37,34 @@ public class ArrayOfLongestString {
 
 }
 }
+
+/*In above, only element with longer string need to be in Array
+ * String[] inputArray = {"aba", "aa", "ad", "vcd", "aba"};
+ * 
+ * AttayList<String> list = new ArrayList<>;
+ * Approach:
+ * 1. create new array with len same as given array
+ * int[] arr = new int[inputArray.length]
+ * 
+ * 2.run loop to add len of element in array
+ * for(int i=0; i<inputArray.length;i++){
+ * arr[i] = inputArray[i].length()
+ *}
+ * 
+ * 3. Once we get array with ele len sort it to get highest len at
+ * end
+ * Arrays.sort(arr);
+ * 
+ *declare maxlength as last ele of array
+ *int max = arr[arr.length-1];
+ * 
+ * 4. After sorting add array ele to list in loop, if ele len is same as max
+ * for(int i=0;i<arr.length;i++){
+ * if(arr[i] == max){
+ * list.add(aarr[i]);
+ * }
+ * }
+ * 
+ * 5. convert list to array and display result
+ * String arr1 = list.toArray(new String[list.size()])
+ */
