fb38f9a91017799c012a344f64a1ccb40af11dda
Nitu Balwani <balwaninitu@gmail.com>
2023-05-17 12:17:28 +0800

    23rd que : MaximizeSum

---
+++ LC/src/KK/MaximizeSum.java
@@ -0,0 +1,40 @@
+package KK;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+
+public class MaximizeSum {
+
+	public static void main(String[] args) {
+		int[] nums = {1,2,3,4,5};
+		int k = 3;
+		int sum = maximizeSum(nums,k);
+		System.out.println(sum);
+
+	}
+	
+public static int maximizeSum(int[] nums, int k) {
+	
+	ArrayList<Integer> list = new ArrayList<>();
+	for (Integer i : nums) {
+		list.add(i);
+	}
+	Collections.sort(list);
+	int sum=0;
+	int len = list.size();
+	
+	while(k>0) {
+		int n = list.get(len-1);
+		sum+=n;
+		list.remove(len-1);
+		list.add(n+1);
+		k--;
+	}
+	 return sum;
+	
+}    
+   
+
+}
